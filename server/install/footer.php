6295ec013648e7cf2f59282eb88f418601ff0fb8
Jason <icryptic@users.noreply.github.com>
2013-12-11 04:22:29 -0700

    Update footer.php

--- server/install/footer.php
+++ server/install/footer.php
@@ -1,13 +1,6 @@
-<TABLE cellSpacing=0 cellPadding=0 width=595px border=0>
-<TBODY>
-<tr><td height=20></td></tr>
-<TR>
-<TD bgColor=#d6d6d6>&nbsp;</TD>
-</TR>
-<TR><TD height=7></TD></TR>
-</TBODY>
-</TABLE>
-<?=$copyright;?> - <?=$application_name;?> <?=$application_version;?> �
+    <br />
+<br />
+<?=$copyright;?> - <?=$application_name;?> <?=$application_version;?>
 <? if($license_agreement_page != ""){ ?>
-    <A href="<?=$license_agreement_page;?>" target=_blank>License</A>
+    <a href="<?=$license_agreement_page;?>" target=_blank>License</a>
 <? } ?>
