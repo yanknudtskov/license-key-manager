4fc0d9a9c5f01ed39c808610b8d3c0455e34dce9
Jason <icryptic@users.noreply.github.com>
2013-12-12 02:19:09 -0700

    Update install.php

--- server/install/install.php
+++ server/install/install.php
@@ -1,14 +1,29 @@
 <?php
 
     require_once("settings.inc");    
-    
+
     if (file_exists($config_file_path)) {        
 		header("location: ".$application_start_file);
         exit;
 	}
-       
-?>	
+        
+    ob_start();
+    phpinfo(-1);
+    $phpinfo = array('phpinfo' => array());
+    if(preg_match_all('#(?:<h2>(?:<a name=".*?">)?(.*?)(?:</a>)?</h2>)|(?:<tr(?: class=".*?")?><t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>(?:<t[hd](?: 
 
+class=".*?")?>(.*?)\s*</t[hd]>(?:<t[hd](?: class=".*?")?>(.*?)\s*</t[hd]>)?)?</tr>)#s', ob_get_clean(), $matches, PREG_SET_ORDER))
+    foreach($matches as $match){
+        if(strlen($match[1]))
+            $phpinfo[$match[1]] = array();
+        elseif(isset($match[3]))
+            $phpinfo[end(array_keys($phpinfo))][$match[2]] = isset($match[4]) ? array($match[3], $match[4]) : $match[3];
+        else
+            $phpinfo[end(array_keys($phpinfo))][] = $match[2];
+    }
+
+    
+?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 
@@ -18,17 +33,17 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
-
 <BODY text=#000000 vLink=#2971c1 aLink=#2971c1 link=#2971c1 bgColor=#cccccc>
     
-<TABLE align="center" width="640px" style="background:#ffffff;padding-left:20px;padding-top:20px;margin-top:20px;margin-bottom:20px;margin-left:auto;margin-right:auto;-webkit-box-shadow:0 6px 9px #999;box-shadow:0 6px 9px #999;border-radius:6px 6px 6px 6px">
+<TABLE align="center" width="640px" style="background:#ffffff;margin-top:20px;padding-left:20px;padding-top:20px;margin-bottom:20px;margin-
+
+left:auto;margin-right:auto;-webkit-box-shadow:0 6px 9px #999;box-shadow:0 6px 9px #999;border-radius:6px 6px 6px 6px">
 <TBODY style="background:#ffffff">
 <TR>
     <TD class=text vAlign=top>
-        <div style="float:right;margin-top:-20px"><img style="width:40px;height:40px" src="<?=$logo;?>" width="40" height="40" /></div>
-        <h1>New Installation of <b><?=$application_name;?></b></h1>
-        
-        Fill in the form below to configure the software to work with your MySQL database.<BR><BR>
+        <div style="float:right;margin-top:-20px"><img style="width:40px;height:40px" src="images/settings.png" width="40" height="40" /></div>
+        <h1>New Installation of <b>License Key Manager</b></h1>        
+                Follow the wizard to setup your database.<BR><BR>
         <TABLE width="100%" cellSpacing=0 cellPadding=0 border=0>
         <TBODY>
         <TR>
@@ -42,82 +57,56 @@
                         <TBODY>
                         <TR>
                             <TD class=text align=left>
-								<b>Step 1. Configure Software</b>
-                            </td>
-                        </tr>
-                        </tbody>
-                        </table>
-                        <br />
-                        
-                        <form method="post" action="install2.php">
-                        <input type="hidden" name="submit" value="step2" />  
-                        <table class=text width="100%" border="0" cellspacing="0" cellpadding="2" class="main_text">
+				<b>Getting System Info</b>
+                            </TD>
+                        </TR>
+                        <tr><td>&nbsp;</td></tr>
                         <tr>
-                            <tr>
-                                <td>&nbsp;DB Host</td>
-                                <td>
-                                    <input type="text" class="form_text" name="database_host" value='<?= $database_host ?>' size="30">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>&nbsp;DB Name</td>
-                                <td>
-                                    <input type="text" class="form_text" name="database_name" size="30" value="<?= $database_name ?>">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>&nbsp;DB Username</td>
-                                <td>
-                                    <input type="text" class="form_text" name="database_username" size="30" value="<?= $database_username ?>">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>&nbsp;DB Password</td>
-                                <td>
-                                    <input type="text" class="form_text" name="database_password" size="30" value="<?= $database_password ?>">
-                                </td>
-                            <tr>
-                                <td>&nbsp;Admin Username</td>
-                                <td>
-                                    <input type="text" class="form_text" name="admin_username" size="30" value="<?= $admin_username ?>">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>&nbsp;Admin Password</td>
-                                <td>
-                                    <input type="text" class="form_text" name="admin_password" size="30" value="<?= $admin_password ?>">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td colspan=2>&nbsp;</td>
-                            </tr>
-                            <tr>
-                                <td colspan=2 align='left'>
-				    <input type="button" class="form_button2" name="btn_cancel" value="Cancel" onclick="document.location.href='index.php'">
-				    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                                    <input type="submit" class="form_button" name="btn_submit" value="Continue">
-                                </td>
-                            </tr>
-                        
+                            <TD class=text align=left>
+                                <UL>
+									<LI>System: <?=$phpinfo['phpinfo']['System'];?></li>                                
+
+    
+                                    <LI>PHP version: <?=$phpinfo['phpinfo']['PHP Version'];?></li>
+                                    <LI>Server API: <?=$phpinfo['phpinfo']['Server API'];?></li>
+                                    <LI>Safe Mode: <?=$phpinfo['PHP Core']['safe_mode'][0];?></li>
+								</UL>
+							</TD>
+                        </TR>
+                        <tr><td>&nbsp;</td></tr>
+						<TR>
+                            <TD class=text align=left>
+								Click on Start button to continue.
+							</TD>
+						</TR>
+						</TBODY>
+                        </TABLE>
+						<br />
+
+						
+                        <table width="100%" border="0" cellspacing="0" cellpadding="2" class="main_text">
+                        <tr>
+                            <td colspan=2 align='left'>
+                                <input type="button" class="form_button" value="Start" name="submit" title="Click to start installation" 
+
+onclick="document.location.href='install.php'">
+                            </td>
                         </table>
-                        </form>                        
-						<br />						
 
 					</TD>
                     <TD></TD>
                 </TR>
                 </TBODY>
                 </TABLE>
+
             </TD>
         </TR>
         </TBODY>
         </TABLE>
-                
-        <? include_once("footer.php"); ?>        
+        <?php include_once("footer.php"); ?>        
     </TD>
 </TR>
 </TBODY>
-</TABLE>
-                  
+</TABLE>          
 </body>
 </html>
