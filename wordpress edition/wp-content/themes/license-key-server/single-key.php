21a62f36b58c4005d0f2f01b0903fe0baf41559b
Jason <icryptic@users.noreply.github.com>
2013-12-12 11:44:35 -0700

    Update single-key.php

--- wordpress edition/wp-content/themes/license-key-server/single-key.php
+++ wordpress edition/wp-content/themes/license-key-server/single-key.php
@@ -1,25 +1,25 @@
-<?php 
-
-//call header
-get_header();
- 
-//if valid key
-if( have_posts() ) {
-
-while ( have_posts() ) : the_post(); 
-
-?>
-
-<h4>LICENSE KEY: <?php the_title(); ?></h4>
-<h1><span style="color:#008000">VALID</span></h1>
-
-<?php 
-
-endwhile;
-
-}
-
-//call footer
-get_footer(); 
-
+<?php 
+
+//call header
+get_header();
+ 
+//if valid key
+if( have_posts() ) {
+
+while ( have_posts() ) : the_post(); 
+
+?>
+
+<h4>LICENSE KEY: <?php the_title(); ?></h4>
+<h1><span style="color:#008000">VALID</span></h1>
+
+<?php 
+
+endwhile;
+
+}
+
+//call footer
+get_footer(); 
+
 ?>
