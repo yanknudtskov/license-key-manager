6ad68cad8f9696b7a703a763d495c414f6c932e0
Jason <icryptic@users.noreply.github.com>
2013-12-12 11:42:39 -0700

    Update dashboard_func.php

--- wordpress edition/wp-content/themes/license-key-server/dashboard_func.php
+++ wordpress edition/wp-content/themes/license-key-server/dashboard_func.php
@@ -1,36 +1,36 @@
-<?php
-
-// main class
-class edf_dashboard {
-	function __construct() { 
-		add_action('admin_menu', array( &$this,'p_edf_register_menu') );
-		add_action('load-index.php', array( &$this,'p_edf_redirect_dashboard') );
-	} 
-
-	function p_edf_redirect_dashboard() {
-		if( is_admin() ) {
-
-			$screen = get_current_screen();
-			if( $screen->base == 'dashboard' ) {
-			wp_redirect( admin_url( 'index.php?page=dashboard_view' ) );	
-
-			}
-		}
-	}
-
-
-	function p_edf_register_menu() {
-	        add_dashboard_page( 'General', 'General', 'read', 'dashboard_view', array( &$this,'p_edf_create_dashboard') );
-
-	}
-
-
-	function p_edf_create_dashboard() {
-		include_once( 'dashboard.php' );
-
-	}
-    }
-
-
-// instantiate plugin's class
-$GLOBALS['plugin_edf_dashboard'] = new edf_dashboard();
+<?php
+
+// main class
+class edf_dashboard {
+	function __construct() { 
+		add_action('admin_menu', array( &$this,'p_edf_register_menu') );
+		add_action('load-index.php', array( &$this,'p_edf_redirect_dashboard') );
+	} 
+
+	function p_edf_redirect_dashboard() {
+		if( is_admin() ) {
+
+			$screen = get_current_screen();
+			if( $screen->base == 'dashboard' ) {
+			wp_redirect( admin_url( 'index.php?page=dashboard_view' ) );	
+
+			}
+		}
+	}
+
+
+	function p_edf_register_menu() {
+	        add_dashboard_page( 'General', 'General', 'read', 'dashboard_view', array( &$this,'p_edf_create_dashboard') );
+
+	}
+
+
+	function p_edf_create_dashboard() {
+		include_once( 'dashboard.php' );
+
+	}
+    }
+
+
+// instantiate plugin's class
+$GLOBALS['plugin_edf_dashboard'] = new edf_dashboard();
