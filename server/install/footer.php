0d47858201c2d775854637032cee22433ff9a275
Jason <icryptic@users.noreply.github.com>
2013-12-12 01:31:41 -0700

    Update footer.php

--- server/install/footer.php
+++ server/install/footer.php
@@ -1,6 +1,4 @@
     <br />
 <br />
-<?=$copyright;?> - <?=$application_name;?> <?=$application_version;?>
-<? if($license_agreement_page != ""){ ?>
-    <a href="<?=$license_agreement_page;?>" target=_blank>License</a>
-<? } ?>
+</body>
+</html>
