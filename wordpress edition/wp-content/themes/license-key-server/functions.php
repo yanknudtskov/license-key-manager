cfb779e43879952fe5ba47865438ff37ae1a463e
Jason <icryptic@users.noreply.github.com>
2013-12-13 04:25:50 -0700

    Update functions.php

--- wordpress edition/wp-content/themes/license-key-server/functions.php
+++ wordpress edition/wp-content/themes/license-key-server/functions.php
@@ -99,7 +99,7 @@ add_action( 'admin_menu', 'adjust_the_wp_menu', 999 );
 function lkm_admin_bar_render() {
     global $wp_admin_bar;
     $wp_admin_bar->remove_menu('comments');
-    echo '<style>#wp-admin-bar-site-name, #wp-admin-bar-new-content, #wp-admin-bar-search {display:none}#collapse-menu{display:none}pre{font-size:12px;background:#F7F7F7;border: 1px dashed #ccc;padding: 10px}#screen-meta-links{display:none}#titlediv div.inside, #preview-action{display:none}</style>';
+    echo '<style>#wp-admin-bar-site-name, #wp-admin-bar-new-content, #wp-admin-bar-search {display:none}#collapse-menu{display:none}pre{font-size:12px;background:#F7F7F7;border: 1px dashed #ccc;padding: 10px}#screen-meta-links{display:none}#titlediv div.inside, #preview-action{display:none}span.inline.hide-if-no-js, span.view{display:none}</style>';
 }
 add_action( 'wp_before_admin_bar_render', 'lkm_admin_bar_render' );
 
