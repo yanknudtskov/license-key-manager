fcaf0bb8429e810cef2edaaa6264ea4e729c7cbf
Jason <icryptic@users.noreply.github.com>
2013-12-12 23:35:33 -0700

    Update header.php

--- wordpress edition/wp-content/themes/license-key-server/header.php
+++ wordpress edition/wp-content/themes/license-key-server/header.php
@@ -1,25 +1,5 @@
-<?php // Creative idea, eh? ;)
+<?php 
+
+//header
+
 ?>
-<html>
-<head>
-<title><?php
-	// Print the <title> tag based on what is being viewed.
-	global $page, $paged;
-	wp_title( '|', true, 'right' );
-
-	// Add the blog name.
-	bloginfo( 'name' );
-
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
-
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) );
-
-	?></title>
-<?php wp_head(); ?>
-</head>
-<body>
