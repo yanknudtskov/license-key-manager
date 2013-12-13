4010ebb8d6491c5ed958c46329ed4bf732befd44
Jason <icryptic@users.noreply.github.com>
2013-12-13 03:49:15 -0700

    Update 404.php

--- wordpress edition/wp-content/themes/license-key-server/404.php
+++ wordpress edition/wp-content/themes/license-key-server/404.php
@@ -1,12 +1,12 @@
-<?php 
-
-//call header
-get_header();
- 
-?>
-INVALID<?php 
-
-//call footer
-get_footer(); 
-
+<?php 
+
+//call header
+get_header();
+
+?>
+INVALID<?php 
+
+//call footer
+get_footer(); 
+
 ?>
